f0dbac6a8909005f2e468e3960243e321605136c
halley <demeha@ssfs.org>
2021-10-06 09:00:30 -0400

    did some else statements

--- maximum.py
+++ maximum.py
@@ -1,5 +1,20 @@
 def max(a,b):
-    if a > b
+    if a > b:
         return a
-    else
+    else:
         return b
+def is_divisible(num1,num2):
+    if int(num1) % int(num2) == 0:
+        return True
+    else:
+        return False
+def main():
+    num = input("Please enter a number to check")
+    check = input("Please enter a number to divide by")
+    if is_divisible(num,check):
+        print(num, "is divisible by", check)
+    else:
+        print(num, "is not divisible by", check)
+
+if __name__ == '__main__':
+    main()
