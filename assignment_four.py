62b8ab0096abb0497fe42bbcdf48a346cad79003
halley <demeha@ssfs.org>
2021-10-19 09:40:31 -0400

    unit four assignment

--- assignment_four.py
+++ assignment_four.py
@@ -1,8 +1,25 @@
+# Halley Deme
+# Last Edit : October 19
+# This program is a blackjack simulator.
+
+
+# this is used for the get card function in order to retrieve a random number betweeen 1-10
 import random
+
 def get_card():
+   """
+This function is used to get a random card.
+   :return: returns a card valued between 1-10
+   """
    k = random.randint(1,10)
    return(k)
 def get_winner(ut,dt):
+   """
+   This function compares the user and dealer's total and decides who wins.
+   :param ut: This is the user's total
+   :param dt: This is the dealer's total
+   :return: This says who has the greater total therefore winning.
+   """
    if ut> dt:
       return "you won!"
    if dt>ut:
@@ -16,13 +33,24 @@ def main():
    user_total += get_card()
    print("your total is now " + str(user_total))
    k = input( "would you like another card?")
-   print(k)
-   if k == yes:
+
+   if k =="yes" :
+      user_total += get_card()
+      print("your total is now " + str(user_total))
+   if user_total > 21:
+      print("Your total is greater than 21, you lost.")
+   else:
+      dealer_total = 0
+      dealer_total= dealer_total + get_card()
+      dealer_total+= get_card()
+      print("The dealer's total is " + str(dealer_total))
+      print(get_winner(user_total,dealer_total))
+
+
+
+
 
 main()
 
 
 
-# def get_winner(ut,pt):
-#     if ut > pt
-#     and ut< 21
